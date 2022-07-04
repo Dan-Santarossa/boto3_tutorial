7123802dce38f35285880c0db5b408a40a823487
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-04 04:56:10 +0000

    added note

--- tutorial1.py
+++ tutorial1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3.7
 import boto3
-
+#create s3 bucket in us-east-2 region
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("totaltech")
+bucket=aws_resource.Bucket("totaltech-private")
 response = bucket.create(
-    ACL='public-read',
+    ACL='private',
     CreateBucketConfiguration={
         'LocationConstraint':'us-east-2'
     },
