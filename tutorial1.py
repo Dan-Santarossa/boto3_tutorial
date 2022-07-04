fbdc232079037715a837ca91d07e02fcd983afbd
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-04 04:22:59 +0000

    created s3 bucket with python so cool

---
+++ tutorial1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.7
+import boto3
+
+aws_resource=boto3.resource("s3")
+bucket=aws_resource.Bucket("totaltech")
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint':'us-east-2'
+    },
+   
+)
+
+print(response)
