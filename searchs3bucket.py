b9c2b312e7b9ad0cef833da8472f7834ed91ace2
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-04 20:40:25 +0000

    added multiple different seaching methods

--- searchs3bucket.py
+++ searchs3bucket.py
@@ -2,7 +2,8 @@ import boto3
 
 resource=boto3.resource("s3")
 
-#print()
+bucket_list=list(resource.buckets.all())
+
 
 for bucket in resource.buckets.all():
-    print(bucket.name)
+    print(bucket_list)
