8855ab63641ea611317f90ac7735117d787f6f9e
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-05 21:52:27 +0000

    list objects from bucket

--- listobjectsboto3.py
+++ listobjectsboto3.py
@@ -1,6 +1,7 @@
 import boto3
 s3_resource=boto3.client("s3")
 
+#list out a dictionary of contents                    #add ["Contents"] to get a list
 objects=(s3_resource.list_objects(Bucket="totaltech"))["Contents"]
 
 for object in objects:
