48c5b65696500bd0bba1cd14fd3a6eeee3487e65
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-05 21:54:35 +0000

    list objects from bucket with notes

--- listobjectsboto3.py
+++ listobjectsboto3.py
@@ -4,5 +4,8 @@ s3_resource=boto3.client("s3")
 #list out a dictionary of contents                    #add ["Contents"] to get a list
 objects=(s3_resource.list_objects(Bucket="totaltech"))["Contents"]
 
+#if len(objects)>0:
+    #print("dummy thing")
+
 for object in objects:
     print(object["Key"])
