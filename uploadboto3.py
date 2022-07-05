ae2159379a088f613bc99fd95bca45aa239ded37
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-05 19:59:08 +0000

    having issue populating from cwd. will ask Z on video call

--- uploadboto3.py
+++ uploadboto3.py
@@ -1,8 +1,25 @@
 import boto3
+import os
+import glob
 
-s3_resource=boto3.client("s3")
+#how to upload single file
+#s3_resource=boto3.client("s3")
+#s3_resource.upload_file(
+    #Filename="testupload.txt",
+    #Bucket="totaltech",
+    #Key="testupload")
+    
+cwd=os.getcwd()
 
-s3_resource.upload_file(
-    Filename="testupload.txt",
+files=glob.glob(cwd+"*.txt")
+cwd=cwd+"/"
+
+for file in files:
+    s3_resource=boto3.client("s3")
+    s3_resource.upload_file(
+    Filename=file,
     Bucket="totaltech",
-    Key="testupload")
+    Key=file.split("/"))
+
+print(cwd)    
+print(files)    
