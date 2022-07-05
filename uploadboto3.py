6153a740209259ff5f74a54500e29861af226a3a
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-05 20:40:42 +0000

    upload multiple files to s3 bucket

--- uploadboto3.py
+++ uploadboto3.py
@@ -3,23 +3,23 @@ import os
 import glob
 
 #how to upload single file
-#s3_resource=boto3.client("s3")
+s3_resource=boto3.client("s3")
 #s3_resource.upload_file(
     #Filename="testupload.txt",
     #Bucket="totaltech",
     #Key="testupload")
-    
-cwd=os.getcwd()
 
-files=glob.glob(cwd+"*.txt")
-cwd=cwd+"/"
+#upload multiple files
+cwd=os.getcwd()
+#cwd=cwd+"/boto3_tutorial/" noted out because cwd did not require additonal folders added
+files=glob.glob(cwd+"/*.txt")
 
 for file in files:
     s3_resource=boto3.client("s3")
     s3_resource.upload_file(
     Filename=file,
     Bucket="totaltech",
-    Key=file.split("/"))
+    Key=file.split("/")[-1])
 
 print(cwd)    
 print(files)    
