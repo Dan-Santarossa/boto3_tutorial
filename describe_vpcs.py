f4eb4e67c19a7f7bcb787ee4abdc9e5da9a9b21e
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-06 16:02:19 +0000

    completed tutorial on descibe vpcs

--- describe_vpcs.py
+++ describe_vpcs.py
@@ -11,10 +11,18 @@ no_of_vpcs=x["Vpcs"]
 
 len(no_of_vpcs)
 
+#print amount of vpcs in my account 
+print(len(no_of_vpcs))
 print(no_of_vpcs)
 print("")
 
+#print specific attributes for my vpcs
 for vpc in no_of_vpcs:
     print(vpc["VpcId"])
     print(vpc["InstanceTenancy"])
     print(vpc["CidrBlock"])
+    
+#how to describe one vpc based on vpc id
+y=client.describe_vpcs(VpcIds=["vpc-0bbf93ba870e43c99"])
+print("")
+print(y)
