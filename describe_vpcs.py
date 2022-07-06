77a3d7f73d3ee2e9a951cae5ce1ac3085360d17a
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-06 15:51:11 +0000

    completed describe vpcs tutorial

--- describe_vpcs.py
+++ describe_vpcs.py
@@ -3,4 +3,18 @@
 
 import boto3
 
+client=boto3.client("ec2")
 
+x=client.describe_vpcs()
+
+no_of_vpcs=x["Vpcs"]
+
+len(no_of_vpcs)
+
+print(no_of_vpcs)
+print("")
+
+for vpc in no_of_vpcs:
+    print(vpc["VpcId"])
+    print(vpc["InstanceTenancy"])
+    print(vpc["CidrBlock"])
