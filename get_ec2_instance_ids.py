765dd405e04d32e1ff9d479365b1c679c0894e43
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 17:25:16 -0400

    tutorial not working. only getting result of one instance see notes in code

---
+++ get_ec2_instance_ids.py
@@ -0,0 +1,11 @@
+import boto3
+import pprint 
+
+ec2_client=boto3.client("ec2")
+x=ec2_client.describe_instances() #describe instances and their details
+data=x["Reservations"][3]#print the details of the first instance. 0 represents the first instance
+data_instance=data["Instances"]
+for i in range (len(data_instance)):
+    print(f"instance id is {data_instance[i]['InstanceId']}")
+
+#only getting the result of one instance ID. it is t he result of whatever int in on the 6th line
