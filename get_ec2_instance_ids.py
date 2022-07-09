11b04f730071b8929e2a1811c7443725f9572603
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 17:48:35 -0400

    found a better way to find and print my instance Id

--- get_ec2_instance_ids.py
+++ get_ec2_instance_ids.py
@@ -1,12 +1,18 @@
 import boto3
 import pprint 
 
-ec2_client=boto3.client("ec2")
-x=ec2_client.describe_instances() #describe instances and their details
-data=x["Reservations"][3]#print the details of the first instance. 0 represents the first instance
-data_instance=data["Instances"]
-for i in range (len(data_instance)):
-    print(f"instance id is {data_instance[i]['InstanceId']}")
+ec2=boto3.client("ec2")
+# x=ec2_client.describe_instances() #describe instances and their details
+# data=x["Reservations"][3]#print the details of the first instance. 0 represents the first instance
+# data_instance=data["Instances"]
+# for i in range (len(data_instance)):
+#     print(f"instance id is {data_instance[i]['InstanceId']}")
 
 #only getting the result of one instance ID. it is t he result of whatever int in on the 6th line
-pprint.pprint(x)
+
+all_ec2_instance = ec2.describe_instances()
+for reservation in all_ec2_instance["Reservations"]:
+    for instance in reservation["Instances"]:
+        print("Instance Id : {}".format(instance["InstanceId"]))
+
+#found a better way anyway^^^^^
