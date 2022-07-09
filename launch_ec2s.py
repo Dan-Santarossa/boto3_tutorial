4c9e42b9004c46548bb62abf7be7453082914f31
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 15:28:09 -0400

    corrected ami-image and min max amount - success on run

--- launch_ec2s.py
+++ launch_ec2s.py
@@ -2,7 +2,7 @@ import boto3
 
 ec2_resource=boto3.resource("ec2")
 
-ec2_resource.create_instances(ImageId='ami-02d1e544b84bf7502',
+ec2_resource.create_instances(ImageId='ami-0cff7528ff583bf9a',
     InstanceType='t2.micro',
-    MaxCount=123,
-    MinCount=123)
+    MaxCount=1,
+    MinCount=1)
