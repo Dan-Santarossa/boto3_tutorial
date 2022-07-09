133754a17c49c7a73a2ee5bf9fc760f910d52627
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 18:39:05 -0400

    create and terminate tutorial

--- create_multiplue_ec2.py
+++ create_multiplue_ec2.py
@@ -1,8 +1,8 @@
 import boto3
 
-ec2_resource=boto3.resource("ec2")
+ec2_resource=boto3.resource("ec2", "us-east-2")#created for us-east-2 obliterate env
 
-ec2_resource.create_instances(ImageId='ami-0cff7528ff583bf9a', 
+ec2_resource.create_instances(ImageId='ami-02d1e544b84bf7502', 
     InstanceType='t2.micro',
-    MaxCount=4,
+    MaxCount=5,
     MinCount=3)
