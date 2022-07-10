ffde408dfd434f618de04263f627530675d53b3f
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 23:42:10 -0400

    fixed duplicate of 'client' in service call

--- delete_ebs_vol.py
+++ delete_ebs_vol.py
@@ -1,9 +1,9 @@
 import boto3 
 ec2_client=boto3.client("ec2")
 
-ec2_client.client.delete_snapshot(
+delete=ec2_client.delete_snapshot(
     SnapshotId='snap-0a802d2ff1e23a914',
     DryRun=True|False
 )
 
-print(ec2_client)
+print(delete)
