40a9e145a0defc43ecd529f514693b517ac86431
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 23:08:54 -0400

    created snap shot

---
+++ ebs_volume_create.py
@@ -0,0 +1,9 @@
+import boto3
+
+#call ec2 client
+ec2_client=boto3.client("ec2")
+
+#create snapshot from PythonLUIT instnace in Cloud9
+ec2_client.create_snapshot(Description='This is my root volume snapshot of PythonLUIT Cloud9.', 
+        VolumeId='vol-0982da85cefc9dc9d',) 
+print(ec2_client)
